e455ee3cdb3b6306b423df771606c4a4e668aad4
Hernan <dario.quiroz@udea.edu.co>
2021-11-01 09:52:13 -0500

    instalacion mongoose

--- servidor.js
+++ servidor.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mongoose = require ('mongoose')
 const app = express();
+
+// conexion con la base de datos
+//mongoose.connect("mongodb+srv://prog_web:ProgWebMintic2022@cluster0.od7lu.mongodb.net/Reto2db?retryWrites=true&w=majority")
+
+// operaciones del CRUD
 
 app.listen(3000, () => {
     console.log("El servidor se esta ejecutando en el puerto 3000")
